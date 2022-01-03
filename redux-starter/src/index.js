afe4fd2186934ecdc2fdba6a7bba87913eacac56
Ali Subhani <ali.subhani@tintash.com>
2022-01-03 18:01:03 +0500

    redux practice

--- redux-starter/src/index.js
+++ redux-starter/src/index.js
@@ -1,1 +1,13 @@
-console.log("Hello World!");
+import store from "./store";
+import * as actions from "./actionTypes";
+import { bugAdded, bugRemoved, bugResolved } from "./actions";
+const unsubscribe = store.subscribe(() => {
+  console.log("store changed!", store.getState());
+});
+
+store.dispatch(bugAdded("bug1"));
+store.dispatch(bugResolved(1));
+
+unsubscribe();
+store.dispatch(bugRemoved(1));
+console.log(store.getState());
